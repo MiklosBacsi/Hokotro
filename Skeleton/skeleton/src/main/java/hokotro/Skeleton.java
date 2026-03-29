ded84e35fe98421a5897d7c200c41415b5211fff
MiklosBacsi <mbacsim@gmail.com>
2026-03-29 21:12:34 +0200

    improve skeleton console output

--- Skeleton/skeleton/src/main/java/hokotro/Skeleton.java
+++ Skeleton/skeleton/src/main/java/hokotro/Skeleton.java
@@ -8,13 +8,14 @@ public class Skeleton {
         int testCase = -1;
         System.out.println("Starting the Hokotro Skeleton…");
         do {
-            System.out.println("Available test-cases:");
+            System.out.println("\nAvailable test-cases:");
+            System.out.println("0. Exit program");
             System.out.println("1. Enter a Lane");
             System.out.println("2. Choosing the lane in a crossing");
             System.out.println("3. Purchasing an item");
             System.out.println("4. Buying a new snowplower");
             System.out.println("5. Changing the snowplower’s head");
-            System.out.print("Choose test-case (1-5):");
+            System.out.print("Choose test-case (1-5):  ");
 
             
             if (scanner.hasNextInt()){
@@ -27,6 +28,7 @@ public class Skeleton {
                     case 4 -> testCase4();
                     case 5 -> testCase5();
                     case 0 -> System.out.println("Exiting...");
+                    default -> System.out.println("Invalid choice, try again.");
                 }
             } else {
                 String input = scanner.next();
